3b0802402f297396267b898143482a5ece5c2c29
forty2 <hereisb612@gmail.com>
2026-09-11 13:32:12 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        arr = [0] * 26
+
+        for c in s:
+            arr[ord(c) - ord("a")] += 1
+
+        for c in t:
+            arr[ord(c) - ord("a")] -= 1
+            if arr[ord(c) - ord("a")] < 0:
+                return False
+
+        return True
